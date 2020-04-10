e2ee06615a7b4ca9e487093337cfa29b70e95e43
nhat301039 <luongminhnhat.03011993@gmail.com>
2020-04-10 11:27:56 +0700

    lam tron

--- app/src/main/java/com/example/random25022020/MainActivity.java
+++ app/src/main/java/com/example/random25022020/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.random25022020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.audiofx.DynamicsProcessing;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       long a = Math.round(1.5);
+        Log.d( "BBB",String.valueOf(a));
     }
 }
